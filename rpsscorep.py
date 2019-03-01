c5c89d4b35c4e2d29f250428dd52262364c33e35
mprathyushareddy1629 <47237484+mprathyushareddy1629@users.noreply.github.com>
2019-03-01 12:11:27 +0530

    Add files via upload

---
+++ rpsscorep.py
@@ -0,0 +1,42 @@
+import random
+l=["r","p","s"]
+us=0
+cs=0
+
+#take input from the user
+u=input("enter the choice,enter q to quit")
+
+#input from the computer
+c=random.choice(l)
+print("computer chooses",c)
+
+#compare the user and the computer choice
+if u==c:
+	#print("tie")
+	cs=us
+elif u=="r" and c=="p":
+	#print("comp wins")
+	print("score is", cs==cs+1)
+elif u=="s" and c=="r":
+	#print("comp win")
+	print("score is",cs==cs+1)
+elif u=="p" and c=="s":
+	#print("comp wins")
+	print("score is",cs==cs+1)
+
+else:
+	#print("you win")
+	us=us+1
+#to quit
+if u=="q":
+	print("game over")
+	print("computer and user score is",cs,us)
+if cs>us:
+	print("computer wins",cs,us)
+elif us>cs:
+	print("user wins",cs,us)
+else:
+	print("its a tie",cs,us)
+
+	exit()
+
