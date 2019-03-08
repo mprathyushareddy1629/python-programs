865a3b635c7e7bbfef6ff257d51f94254a5031a4
mprathyushareddy1629 <47237484+mprathyushareddy1629@users.noreply.github.com>
2019-03-08 12:42:33 +0530

    Add files via upload

---
+++ modules.py
@@ -0,0 +1,13 @@
+#modules
+def add(a,b):
+	c=a+b
+	return c;
+def sub(a,b):
+	c=a-b
+	return c;
+def mult(a,b):
+	c=a*b
+	return c;
+def div(a,b):
+	c=a/b
+	return c;
---
+++ prathumodule.py
@@ -0,0 +1,9 @@
+import modules
+s=modules.add(2,8)
+print(s)
+s=modules.sub(7,4)
+print(s)
+s=modules.mult(6,78)
+print(s)
+s=modules.div(78,3)
+print(s)
